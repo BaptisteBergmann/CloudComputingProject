23ce7f3c4068ec4ca91020aa055d8b14b088a162
Baptiste Bergmann <bergmann.baptiste@gmail.com>
2021-12-16 11:22:01 -0500

    terraform changes

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # CloudComputingProject
 This is a cloud project for the log8415 class
+
+## Main instance
+Commands to install:
+
+# Terraform
+
+$ curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+$ sudo apt-add-repository "deb [arch=$(dpkg --print-architecture)] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+$ sudo apt install terraform
+$ terraform version
+
+# Ansible
+I install them by the ubuntu packet manager instead of pip to be able to easy update it
+
+$ sudo apt update
+$ sudo apt install software-properties-common
+$ sudo add-apt-repository --yes --update ppa:ansible/ansible
+$ sudo apt install ansible
+## Ressources
+https://ankush-chavan.medium.com/integrating-ansible-with-terraform-to-make-a-powerful-infrastructure-50795c36f78b
+https://www.scottyfullstack.com/blog/devops-01-aws-terraform-ansible-jenkins-and-docker/
+
--- mainInstance/terraform/main.tf
+++ mainInstance/terraform/main.tf
@@ -4,22 +4,28 @@ region = "us-east-1"
 
 variable "awsprops" {
     type = "map"
-    default = {
+  api = {
     region = "us-east-1"
-    vpc = "vpc-5234832d"
-    ami = "ami-0c1bea58988a989155"
+    vpc = "vpc-032556978aaa97712"
+    ami = "ami-04505e74c0741db8d"
     itype = "t2.micro"
-    subnet = "subnet-81896c8e"
-    publicip = true
     keyname = "myseckey"
-    secgroupname = "IAC-Sec-Group"
+    secgroupname = "log8415-API"
+  }
+  worker = {
+    region = "us-east-1"
+    vpc = "vpc-032556978aaa97712"
+    ami = "ami-04505e74c0741db8d"
+    itype = "t2.medium"
+    keyname = "myseckey"
+    secgroupname = "log8415-Worker"
   }
 }
 
-resource "aws_security_group" "project-iac-sg" {
-  name = lookup(var.awsprops, "secgroupname")
-  description = lookup(var.awsprops, "secgroupname")
-  vpc_id = lookup(var.awsprops, "vpc")
+resource "aws_security_group" "log8415-API" {
+  name = lookup(var.awsprops.api, "secgroupname")
+  description = lookup(var.awsprops.api, "secgroupname")
+  vpc_id = lookup(var.awsprops.api, "vpc")
 
   // To Allow SSH Transport
   ingress {
@@ -49,33 +55,87 @@ resource "aws_security_group" "project-iac-sg" {
   }
 }
 
-resource "aws_instance" "project-iac" {
-  ami = lookup(var.awsprops, "ami")
-  instance_type = lookup(var.awsprops, "itype")
-  subnet_id = lookup(var.awsprops, "subnet") #FFXsubnet2
-  associate_public_ip_address = lookup(var.awsprops, "publicip")
-  key_name = lookup(var.awsprops, "keyname")
+resource "aws_security_group" "log8415-WORKER" {
+  name = lookup(var.awsprops.worker, "secgroupname")
+  description = lookup(var.awsprops.worker, "secgroupname")
+  vpc_id = lookup(var.awsprops.worker, "vpc")
+
+  // To Allow SSH Transport
+  ingress {
+    from_port = 22
+    protocol = "tcp"
+    to_port = 22
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  // To Allow Port 80 Transport
+  ingress {
+    from_port = 80
+    protocol = ""
+    to_port = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+
+resource "aws_instance" "log8415-API" {
+  ami = lookup(var.awsprops.api, "ami")
+  instance_type = lookup(var.awsprops.api, "itype")
+  key_name = lookup(var.awsprops.api, "keyname")
 
 
   vpc_security_group_ids = [
-    aws_security_group.project-iac-sg.id
+    aws_security_group.log8415-API.id
   ]
   root_block_device {
     delete_on_termination = true
-    iops = 150
-    volume_size = 50
+    volume_size = 8
     volume_type = "gp2"
   }
   tags = {
     Name ="SERVER01"
     Environment = "DEV"
     OS = "UBUNTU"
-    Managed = "IAC"
+    Managed = "API"
   }
 
-  depends_on = [ aws_security_group.project-iac-sg ]
+  depends_on = [ aws_security_group.log8415-API ]
 }
 
+resource "aws_instance" "log8415-WORKER" {
+  ami = lookup(var.awsprops.worker, "ami")
+  instance_type = lookup(var.awsprops.worker, "itype")
+  key_name = lookup(var.awsprops.worker, "keyname")
+
+
+  vpc_security_group_ids = [
+    aws_security_group.log8415-WORKER.id
+  ]
+  root_block_device {
+    delete_on_termination = true
+    volume_size = 8
+    volume_type = "gp2"
+  }
+  tags = {
+    Name ="SERVER01"
+    Environment = "DEV"
+    OS = "UBUNTU"
+    Managed = "WORKER"
+  }
+
+  depends_on = [ aws_security_group.log8415-WORKER ]
+}
 
 output "ec2instance" {
   value = aws_instance.project-iac.public_ip
